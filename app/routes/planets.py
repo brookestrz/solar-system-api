6d3debd9365925e8c7f79541725e08a847e2f5cd
Brooke hill <brookeannstasia@gmail.com>
2022-10-25 13:51:39 -0400

    Finished wave 2

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,abort,make_response
 
 class Planet:
     def __init__(self,id,name,description,known_moons):
@@ -27,11 +27,27 @@ def get_all_planets():
     return jsonify(planets_response)
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id): 
-    planet_id = int(planet_id)
-    for planet in PLANETS: 
+def handle_planet(planet_id):
+
+    return validate_planet(planet_id)
+    
+    #planet_id = int(planet_id)
+    #for planet in PLANETS: 
+        #if planet.id == planet_id:
+            #return { "id": planet.id, "description": planet.description, 
+                    #"known_moons": planet.known_moons
+                
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in PLANETS:
         if planet.id == planet_id:
-            return { "id": planet.id, "description": planet.description, 
-                    "known_moons": planet.known_moons
-                }
+            return vars(planet)
+
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
 
